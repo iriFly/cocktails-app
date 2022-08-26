3a103c0dbb7137c4e019fb7f37f98885039c00ba
Irina Afanassenko <irina.afanassenko@gmail.com>
2022-08-26 12:37:40 +0200

    ingredient detail list corrected

--- cocktails-app-start/src/app/ingredient.model.ts
+++ cocktails-app-start/src/app/ingredient.model.ts
@@ -3,8 +3,5 @@ export interface Ingredient {
   name: string;
   category: string;
   instructions: string;
-  alcoholic: string;
-  glass: string;
-  imageUrl: string;
   ingredients: Array<string>;
 }
