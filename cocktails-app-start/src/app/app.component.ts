4ac81866f9a7c415bb0a879840dfdcff57fc0ca4
Irina Afanassenko <irina.afanassenko@gmail.com>
2022-08-26 11:09:48 +0200

    mock authentication

--- cocktails-app-start/src/app/app.component.ts
+++ cocktails-app-start/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AuthService } from './services/auth.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  loggedIn: boolean;
   title = 'cocktails-app-start';
+
+  constructor(private auth: AuthService){}
+
+  logIn(){
+    this.auth.logIn();
+    this.loggedIn = true;
+  }
+
+  logOut(){
+    this.auth.logOut();
+    this.loggedIn = false;
+  }
+
 }
---
+++ cocktails-app-start/src/app/services/auth.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  private authenticated: boolean;
+
+  constructor() { }
+
+  isAuthenticated(){
+    return this.authenticated;
+  }
+
+  logIn(){
+    this.authenticated = true;
+  }
+
+  logOut(){
+    this.authenticated = false;
+  }
+
+}
